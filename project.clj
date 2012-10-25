d7b7d5953723a13509492728d1af4e1e91ff3b98
Yogthos <dmitri.sotnikov@gmail.com>
2012-10-25 08:57:58 -0400

    added ClojureScript support

--- project.clj
+++ project.clj
@@ -1,5 +1,18 @@
-(defproject tetris "1.0"
-  :description "Tetris example in Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :main tetris)
+(defproject cljs-tetris "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-cljsbuild "0.2.9"]]
+  
+  :source-paths ["src"]
+  ;;cljsbuild does not like cljs files being in the root source folder
+  :cljsbuild {:crossovers [tetris.game]
+              :builds 
+              [{:source-path "src-cljs"
+                :compiler 
+                {:output-to "js/tetris.js"
+                 :optimizations :whitespace
+                 :pretty-print true}}]})
---
+++ src/tetris/core.clj
@@ -0,0 +1,135 @@
+(ns tetris.core
+  (:use tetris.game)
+  (:import
+    (javax.swing JFrame)
+    (java.awt Canvas Font Graphics Color Toolkit)
+    (java.awt.event ActionListener KeyListener KeyEvent)))
+
+;;;;;;Controls;;;;
+(defn handle-input [#^KeyEvent event]
+  (condp = (.getKeyCode event)
+    KeyEvent/VK_LEFT (swap! OFFSET #(map + [-1 0] %))
+    KeyEvent/VK_RIGHT (swap! OFFSET #(map + [1 0] %))
+    KeyEvent/VK_UP (reset! ROTATION :left)
+    KeyEvent/VK_DOWN (reset! ROTATION :right)))
+
+(defn input-listener []
+  (proxy [ActionListener KeyListener] []
+    (actionPerformed [e])
+    (keyPressed [e] (handle-input e))
+    (keyReleased [e])
+    (keyTyped [e])))
+
+;;;;;;;UI;;;;;;;;;
+(def colors {"black"  Color/black
+             "blue"   Color/blue
+             "green"  Color/green
+             "yellow" Color/yellow
+             "orange" Color/orange
+             "pink"   Color/pink
+             "red"    Color/red})
+
+(defn draw [#^Canvas canvas draw-fn]
+  (let [buffer (.getBufferStrategy canvas)
+        g (.getDrawGraphics buffer)]
+    (try
+      (draw-fn g)
+      
+      (finally (.dispose g)))
+    (if (not (.contentsLost buffer))
+      (. buffer show))
+    (.. Toolkit (getDefaultToolkit) (sync))))
+
+(defn draw-square [x y color #^Graphics g]
+  (let [width (/ @WIDTH COLS)
+        height (/ @HEIGHT ROWS)
+        xpos (* x width)
+        ypos (* y width)]    
+    (doto g
+      (.setColor (get colors color))
+      (.fillRect xpos ypos width height)
+      (.setColor Color/black)
+      (.drawRect xpos ypos width height))))
+
+(defn draw-text [#^Graphics g color text x y]
+  (doto g
+    (.setColor color)
+    (.drawString text x y)))
+
+(defn draw-game-over [score]
+  (fn [#^Graphics g]
+    (doto g
+      (.setColor (new Color (float 0) (float 0) (float 0) (float 0.7)))
+      (.fillRect 0 0 @WIDTH @HEIGHT))
+    (draw-text g Color/red "GAME OVER" (- (/ @WIDTH 2) 50) (/ @HEIGHT 2))
+    (draw-text g Color/red (str "Final Score: " score) (- (/ @WIDTH 2) 55) (+ 15 (/ @HEIGHT 2)))))
+
+(defn draw-board [board block score]
+  (fn [#^Graphics g]
+    (doto g
+      (.setColor Color/BLACK)
+      (.fillRect 0 0 @WIDTH @HEIGHT))
+    
+    (doseq [square (range (count board))]
+      (let [[x y] (pos-to-xy square)]
+        (draw-square x y (get board square) g)))
+    
+    (doseq [[x y] (:shape block)]
+      (draw-square x y (:color block) g))
+    
+    (draw-text g Color/green (str "score: " score) 20 25)))
+
+(defn -main [& args]
+  (reset! WIDTH 300)
+  (reset! HEIGHT 600)
+  (let [frame (JFrame. "Tetris")
+        canvas (Canvas.)]
+    (doto frame
+      (.setSize @WIDTH (+ (/ @HEIGHT ROWS) @HEIGHT))
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setResizable false)
+      (.add canvas)
+      (.setVisible true))
+    
+    (doto canvas
+      (.createBufferStrategy 2)
+      (.addKeyListener (input-listener))
+      (.setVisible true)
+      (.requestFocus))
+    
+    ;;game loop
+    (loop [score 0
+           board (get-board)
+           block (get-block)
+           old-time (System/currentTimeMillis)]
+      
+      (reset! OFFSET [0 0])
+      (reset! ROTATION nil)
+      (Thread/sleep 10)
+      (draw canvas (draw-board board block score))
+      
+      (let [cur-time (System/currentTimeMillis)
+            new-time (long (if (> (- cur-time old-time) 250)
+                             cur-time
+                             old-time))
+            drop? (> new-time old-time)
+            [num-removed new-board] (clear-lines board)]
+        (cond
+          (game-over? board)
+          (draw canvas (draw-game-over score))
+          
+          (collides? board (:shape block))
+          (recur
+            score
+            (update-board board block)
+            (get-block)
+            new-time)
+          
+          :default
+          (recur
+            (+ score (* num-removed num-removed))
+            new-board
+            (transform board block drop?)
+            new-time))))))
+
+;(-main)
